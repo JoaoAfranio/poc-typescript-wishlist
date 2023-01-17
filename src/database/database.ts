b294e6305e6139ca9a781f8e9bab62c9598aa552
JoaoAfranio <joaoafraniorosa@hotmail.com>
2023-01-17 18:57:32 -0300

    fixing ssl for deploy

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,9 +6,9 @@ const { Pool } = pg;
 
 const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default connection;
